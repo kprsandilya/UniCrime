afbe7b51fcfd442bcbaf409e5cdd67336a7d1d38
kambhani <63814718+kambhani@users.noreply.github.com>
2026-02-28 14:28:18 -0600

    Add supabase

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -15,13 +15,8 @@ import { CrimeLog } from './crime-log/crime-log.entity';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST ?? 'localhost',
-      port: parseInt(process.env.DB_PORT ?? '5432', 10),
-      username: process.env.DB_USER ?? 'postgres',
-      password: process.env.DB_PASSWORD ?? 'postgres',
-      database: process.env.DB_NAME ?? 'unicrime',
+      url: process.env.DATABASE_URL,
       entities: [CrimeLog],
-      synchronize: process.env.NODE_ENV !== 'production',
     }),
     GraphQLModule.forRoot<MercuriusDriverConfig>({
       driver: MercuriusDriver,
